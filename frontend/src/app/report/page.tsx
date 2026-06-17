a0d4fcb4c7ca5246d2def335837188986963956e
I4n1ce <ianiceboutaba@gmail.com>
2026-06-17 16:25:53 +0200

    feat: ReportCard

---
+++ frontend/src/app/report/page.tsx
@@ -0,0 +1,9 @@
+import { ReportForm } from '../../components/ReportCard/ReportForm';
+
+export default function ReportPage() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center p-4">
+      <ReportForm />
+    </div>
+  );
+}
